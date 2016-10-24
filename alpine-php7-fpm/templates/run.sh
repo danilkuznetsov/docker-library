cb02d1898965fd3ed5ddfaa1ddd119553afceb6e
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2016-10-24 13:47:33 +0300

    add posix extensions and re-create tmp/session dir

--- alpine-php7-fpm/templates/run.sh
+++ alpine-php7-fpm/templates/run.sh
@@ -19,6 +19,14 @@ if [ ! -d $VOLUME_LOGS/phpfpm7 ] ; then
 	mkdir -p $VOLUME_LOGS/php-fpm
 fi	
 
+# Enabling session files
+if [ ! -d $VOLUME_RUN/tmp/sessions/ ] ; then
+	mkdir -p $VOLUME_RUN/tmp/sessions/
+	chown -Rf www-data:www-data /tmp/sessions 
+	chmod g+x $VOLUME_RUN/tmp/sessions/
+	sed -i -e "s:;\s*session.save_path\s*=\s*\"N;/path\":session.save_path = /DATA/phpfpm7/tmp/sessions:g" /etc/php7/php.ini
+fi
+
 echo "=> Set and check permissions"
 chown -R www-data:www-data /DATA/
 
