d60bfc12b7f9d873ea51c1ac797bbc393829bd96
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-05-04 19:55:02 +0300

    add geoip module and small refactoring

--- alpine-nginx/templates/run.sh
+++ alpine-nginx/templates/run.sh
@@ -4,7 +4,7 @@ VOLUME_LOGS="/DATA/logs"
 VOLUME_CONF="/DATA/conf/nginx"
 
 if [[ -f $VOLUME_CONF/nginx.conf ]]; then
-	echo "=> Creating link on Nginx config files"
+	echo "=> Creating link on main Nginx config"
 	rm -rf /etc/nginx/nginx.conf
 	ln -sf $VOLUME_CONF/nginx.conf /etc/nginx/nginx.conf
 fi
@@ -21,11 +21,11 @@ if [ ! -d $VOLUME_LOGS/nginx ]; then
 fi 
 
 echo "=> Set permissions /DATA/"
-chown -R www-data:www-data /DATA/
+chown -R nginx:nginx /DATA/
 
 echo "=> Check and create tmp folder"
 mkdir -p /tmp/nginx
-chown www-data:www-data /tmp/nginx
+chown nginx:nginx /tmp/nginx
 
 echo "=> Start nginx"
-nginx
+exec "$@"
